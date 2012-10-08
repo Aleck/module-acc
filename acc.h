d68b4533257a539e700a2baba34df14f4979278f
Aleck <gadio87@gmail.com>
2012-10-08 16:29:39 +0200

    Aggiunto le operazioni semplici, ma non vanno

--- acc.h
+++ acc.h
@@ -28,10 +28,10 @@
 
 //dichiaro le strutture necessarie
 struct acc_dev {
-	void *data;               /* Puntatore al dato            */
-	int size;                 /* la dimensione del dato       */
-	struct semaphore sem;     /* mutual exclusion semaphore   */
-	struct cdev cdev;	  /* Char device structure	  */
+	void *data;               /* Puntatore al dato              */
+	int size;                 /* la dimensione del dato corrente*/
+	struct semaphore sem;     /* mutual exclusion semaphore     */
+	struct cdev cdev;	  /* Char device structure	    */
 };
 
 
@@ -51,6 +51,11 @@ void gestisci_errore(int errore);
 //dichiaro i prototipi delle funzioni legate ai moduli
 int acc_init_module(void);
 void acc_cleanup_module(void);
+int acc_open (struct inode *inode, struct file *filp);
+int acc_trim(struct acc_dev *dev);
+int acc_release (struct inode *inode, struct file *filp);
+ssize_t acc_read (struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
+ssize_t acc_write (struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
 
 
 
