51fb819c46b31ff88ce524113963409757c0302e
OpenRISC user <openrisc@openrisc-VirtualBox.(none)>
2012-10-14 13:12:31 +0200

    modificato il file .cfg con l'aggiunta di una memoria riservata all'acceleratore

--- simulatore/prova.c
+++ simulatore/prova.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(){
 	int op_1 = 1;
 	int op_2 = 3;
 	int risultato = 0;
 	int parti = 0;
-	int *ptr1;
-	int *ptr2;
-	int *ptr3;
-	int *ptr0;
-	ptr1 = 0x02000001; 
-	ptr2 = 0x02000002;
-	ptr3 = 0x02000003;
-	ptr0 = 0x02000000;
-	*ptr1 = op_1;
-	*ptr2 = op_2;
-	*ptr0 = parti;
+	int mem = open("/dev/acc", O_RDWR);
+	int w1 = write(mem, &op_1, sizeof(int));
+	int w2 = write(mem, &op_2, sizeof(int));
+	int w3 = write(mem, &parti, sizeof(int));
+	printf("le dimensioni delle write(in bytes) sono: %i, %i e %i .\n", w1, w2, w3);
 	while(1){
-		if(*ptr0 = 1){
+		
+		if(){
 			*ptr0 = 0;
 			risultato = *ptr3;
 			break;
 		}	
 		delay(10);
 	}
-	printf("il risultato ottenuto è: %i", risultato);
+	printf("il risultato ottenuto è: %i. \n", risultato);
+	close(mem);
 	return 0;
 }
